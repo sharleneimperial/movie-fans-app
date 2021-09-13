9033b22e2348c96c61450d997cf5b67de73a9017
Sharleneimperial <Sharlene_imperial@yahoo.com>
2021-09-13 15:35:02 -0700

    app

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 const flash = require('connect-flash');
 const session = require('express-session');
 const passport = require('./config/ppConfig');
-const authorizedRoute = require('./middleware/isloggedIn');
+const isLoggedIn = require('./middleware/isLoggedIn');
 const methodOverride = require('method-override');
 
 const SECRET_SESSION = process.env.SECRET_SESSION;
